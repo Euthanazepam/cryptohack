680f103318afca0b31033fa7361b81d365fd5cd0
Euthanazepam <euthanazepam@protonmail.com>
2024-06-30 01:17:37 +0300

    #add #introduction - hex

---
+++ Introduction to CryptoHack/02_hex.py
@@ -0,0 +1,14 @@
+def get_flag() -> str:
+    """
+    https://cryptohack.org/courses/intro/enc2/
+    """
+
+    hex_string = '63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d'
+
+    flag = bytes.fromhex(hex_string).decode('utf-8')
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
