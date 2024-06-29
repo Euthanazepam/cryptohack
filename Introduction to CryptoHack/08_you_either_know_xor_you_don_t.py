089754307939a2a3baaca355758fbad21264fc5c
Euthanazepam <euthanazepam@protonmail.com>
2024-06-30 02:20:24 +0300

    #add #introduction - you either know, xor you don't

---
+++ Introduction to CryptoHack/08_you_either_know_xor_you_don_t.py
@@ -0,0 +1,32 @@
+from pwn import xor
+
+
+def get_flag() -> str:
+    """
+    https://cryptohack.org/courses/intro/xorkey1/
+    """
+
+    hex_string = '0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104'
+
+    message = bytes.fromhex(hex_string)
+
+    """
+    Two things are known:
+    1. Text of message.
+    2. The beginning of flag is b'crypto{'.
+    So, secret_key = message ⊕ b'crypto{'.
+    """
+
+    expected_result = b'crypto{'
+
+    # This fucking piece of shit is needed to remove the '+' symbol from the secret key.
+    # Yes, I know. This is a very clumsy method.
+    secret_key = xor(message[:9], expected_result).decode('utf-8').replace('+', '').encode()
+
+    flag = xor(message, secret_key).decode('utf-8')
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
