82341f9ed90e545580d47a2d8ba46e1e7885cf2c
Euthanazepam <euthanazepam@protonmail.com>
2024-06-30 02:30:26 +0300

    #add #modular - extended GCD

---
+++ Modular Arithmetic/02_extended_gcd.py
@@ -0,0 +1,22 @@
+def get_flag() -> int:
+    """
+    https://cryptohack.org/courses/modular/egcd/
+    """
+
+    a = 26513
+    b = 32321
+
+    # Snatch from https://brilliant.org/wiki/extended-euclidean-algorithm.
+    x, y, u, v = 0, 1, 1, 0
+    while a != 0:
+        q, r = b // a, b % a
+        m, n = x - u * q, y - v * q
+        b, a, x, y, u, v = a, r, u, v, m, n
+    gcd = b
+
+    # return gcd, x, y
+    return min(x, y)
+
+
+if __name__ == '__main__':
+    print(get_flag())
