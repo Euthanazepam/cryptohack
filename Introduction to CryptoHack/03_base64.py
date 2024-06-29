acdebecd00710d56fb401c7faf4fc7f1f7193e92
Euthanazepam <euthanazepam@protonmail.com>
2024-06-30 01:23:51 +0300

    #add #introduction - base64

---
+++ Introduction to CryptoHack/03_base64.py
@@ -0,0 +1,17 @@
+import base64
+
+
+def get_flag() -> str:
+    """
+    https://cryptohack.org/courses/intro/enc3/
+    """
+
+    hex_string = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
+
+    flag = base64.b64encode(bytes.fromhex(hex_string)).decode('utf-8')
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
