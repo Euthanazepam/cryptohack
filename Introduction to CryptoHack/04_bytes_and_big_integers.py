c9ceb391d1f8aafac77946f7c11ceddce7df93c9
Euthanazepam <euthanazepam@protonmail.com>
2024-06-30 01:29:11 +0300

    #add #introduction - bytes and big integers

---
+++ Introduction to CryptoHack/04_bytes_and_big_integers.py
@@ -0,0 +1,17 @@
+from Crypto.Util.number import long_to_bytes
+
+
+def get_flag() -> str:
+    """
+    https://cryptohack.org/courses/intro/enc4/
+    """
+
+    integer = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+    flag = long_to_bytes(integer).decode('utf-8')
+
+    return flag
+
+
+if __name__ == '__main__':
+    print(get_flag())
