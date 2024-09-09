29da14fc4d66bce2f6268a33d36e1d4513f5ff8c
Euthanazepam <euthanazepam@protonmail.com>
2024-09-09 18:08:48 +0000

    #add #public-key - factoring

---
+++ Public-Key Cryptography/07_factoring.py
@@ -0,0 +1,22 @@
+from factordb.factordb import FactorDB
+
+
+def get_flag() -> int:
+    """
+    Returns the challenge flag https://cryptohack.org/courses/public-key/rsa_factoring/
+
+    :return: Flag
+    """
+
+    N = 510143758735509025530880200653196460532653147
+
+    # Use http://factordb.com to factorize N
+    f = FactorDB(N)
+    f.connect()
+    p, q = f.get_factor_list()
+
+    return min(p, q)
+
+
+if __name__ == "__main__":
+    print(get_flag())
